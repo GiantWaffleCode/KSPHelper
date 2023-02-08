dbcb6a8d985f22e0543defe7e7cad24f155e4830
GiantWaffleCode <72772325+GiantWaffleCode@users.noreply.github.com>
2023-02-07 22:10:53 -0600

    Converted from lists to select options
    Redid all the HTML and CSS

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ function generateMoonList() {
     //console.log(selectedPlanet.outerText); //This is the text of the Planet
 
     planetList.forEach((planet)=>{
-        if (planet.name == selectedPlanet.innerText) {
+        if (planet.name == selectedPlanet.outerText) {
             //console.log('Selected Planet is...');
             //console.log(planet.name);
  
@@ -52,27 +52,46 @@ function generateMoonList() {
                 li.innerText = item.name;
                 moonUL.appendChild(li);
             }); 
+
+            //TODO: ADD event listener for click
         };
     });
+};
+
+function updateUL(listItem) {
+    listItem.addEventListener('click', () => {
+        const { select, caret, menu, options, selected} = genQuery(dropdown);
+        selected.innerText = option.innerText;
+        select.classList.remove('select-clicked');
+        caret.classList.remove('caret-rotate');
+        menu.classList.remove('menu-open');
+        options.forEach(option => {
+            option.classList.remove('active');
+        });
+        option.classList.add('active');
+    });
+};
+
+function genQuery(obj) { //generate queries
+    const select = obj.querySelector('.select');
+    const caret = obj.querySelector('.caret');
+    const menu = obj.querySelector('.menu');
+    const options = obj.querySelectorAll('.menu li');
+    const selected = obj.querySelector('.selected');
+
+    return { select, caret, menu, options, selected }; //return obj of queries
 };
 
 
 const dropdowns = document.querySelectorAll('.dropdown');
 
 dropdowns.forEach(dropdown => {
-
-    const select = dropdown.querySelector('.select');
-    const caret = dropdown.querySelector('.caret');
-    const menu = dropdown.querySelector('.menu');
-    const options = dropdown.querySelectorAll('.menu li');
-    const selected = dropdown.querySelector('.selected');
-
+    const { select, caret, menu, options, selected} = genQuery(dropdown);
     select.addEventListener('click', () => {
         select.classList.toggle('select-clicked');
         caret.classList.toggle('caret-rotate');
         menu.classList.toggle('menu-open');   
     });
-
     options.forEach(option => {
         option.addEventListener('click', () => {
             selected.innerText = option.innerText;
